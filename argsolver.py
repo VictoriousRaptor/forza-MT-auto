319bc0726030fee937f47297db6d6e9b8993690f
Vic <dlqranger@gmail.com>
2022-01-06 22:38:36 +0800

    delta

--- argsolver.py
+++ argsolver.py
@@ -18,13 +18,13 @@ parser.add_argument('--handel', help='手离', type=str, default="f8")
 # 注意!!! 改键去keyboardsim.py看一眼键盘码表全不全 不全的自己补一下
 # 注意!!! 改键去keyboardsim.py看一眼键盘码表全不全 不全的自己补一下
 
-parser.add_argument('--clutch', type=str, help='离合按键', default="i")
+parser.add_argument('--clutch', type=str, help='离合按键', default="shift")
 parser.add_argument('--upgear', type=str, help='升档', default="e")
 parser.add_argument('--downgear', type=str, help='降档', default="q")
-parser.add_argument('--clutchBefore', type=float, help='踩下离合到换挡的延迟(秒)', default=0.1)
-parser.add_argument('--clutchAfter', type=float, help='换挡到抬起离合的延迟(秒)', default=0.1)
-parser.add_argument('--downGearCoolDown', type=float, help='降档的cd 连续降档间隔时长(秒)', default=0.5)
-parser.add_argument('--upGearCoolDown', type=float, help='升档的cd 连续升档间隔时长(秒)', default=1)
+parser.add_argument('--clutchBefore', type=float, help='踩下离合到换挡的延迟(秒)', default=0.05)
+parser.add_argument('--clutchAfter', type=float, help='换挡到抬起离合的延迟(秒)', default=0.05)
+parser.add_argument('--downGearCoolDown', type=float, help='降档的cd 连续降档间隔时长(秒)', default=0.1)
+parser.add_argument('--upGearCoolDown', type=float, help='升档的cd 连续升档间隔时长(秒)', default=0.5)
 parser.add_argument('--playerCoolDown', type=float, help='玩家介入换挡后程序发呆的时间(秒)', default=5)
 # 降档时下面两条并行
 # if accelAfterGearDown>0 按离合 sleep(accelBeforeGearDown) 按油门 sleep(accelAfterGearDown) 抬油门
